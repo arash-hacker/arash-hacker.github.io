38d3e105ee6fd77af39aee78a1aac90856ca82f3
ArashMidos <arshiamidos@gmail.com>
2019-09-13 21:42:54 +0430

    add iguana react like

---
+++ demos/build/precache-manifest.6a7b72ac79a7b125c35760832ddce5bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31caecd19940a067fbd74d53937f0a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ada958835bc7c50cc05e",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1063804f2fe7d787d1cc",
+    "url": "./static/js/2.69597924.chunk.js"
+  },
+  {
+    "revision": "ada958835bc7c50cc05e",
+    "url": "./static/js/main.7abe826f.chunk.js"
+  },
+  {
+    "revision": "2752e32a82fd8422e40f",
+    "url": "./static/js/runtime~main.6ce542c9.js"
+  }
+]);
