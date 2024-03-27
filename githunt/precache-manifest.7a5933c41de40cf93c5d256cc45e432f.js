259c3495a7d51d5100574b5b6ce1ba5fd926d71a
Arash hacker <arshiamidos@gmail.com>
2024-03-27 14:04:37 +0100

    chore: added githunt

---
+++ githunt/precache-manifest.7a5933c41de40cf93c5d256cc45e432f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e8035d18331f6e313c93826ad100a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdb753ce8dc2f73fa0ef",
+    "url": "/static/css/2.53a60f20.chunk.css"
+  },
+  {
+    "revision": "98867b41f4c94a1d5270",
+    "url": "/static/css/main.5b277643.chunk.css"
+  },
+  {
+    "revision": "cdb753ce8dc2f73fa0ef",
+    "url": "/static/js/2.af73dbb9.chunk.js"
+  },
+  {
+    "revision": "b22fd9b4a3d7ad3f113bfdae2fd33eec",
+    "url": "/static/js/2.af73dbb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98867b41f4c94a1d5270",
+    "url": "/static/js/main.d88f6a38.chunk.js"
+  },
+  {
+    "revision": "8bcc42190d84685d73fe",
+    "url": "/static/js/runtime-main.43d88c18.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
